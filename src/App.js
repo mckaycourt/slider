1c0a33882a881ee309007d0c3969ac83dc4c5e24
McKay Court <mckay.court@gmail.com>
2018-12-14 10:11:15 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import info from './info';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            sliders: info.sliders,
+            position: 0,
+        };
+    }
+
+    increment = () => {
+        if(this.state.position === this.state.sliders.length -1){
+            this.setState((prevState) => ({
+                position: 0
+            }));
+        }
+        else {
+            this.setState((prevState) => ({
+                position: prevState.position + 1
+            }));
+        }
+    };
+
+    render() {
+        return (
+            <div className='slider'>
+                <h2>{this.state.sliders[this.state.position].title}</h2>
+                <div className='content'>
+                    <img alt="" src={this.state.sliders[this.state.position].imgSrc}/>
+                    <h3>{this.state.sliders[this.state.position].subTitle}</h3>
+                    <p>{this.state.sliders[this.state.position].text}</p>
+                </div>
+                <button onClick={this.increment}>Next</button>
+            </div>
+        );
+    }
 }
 
 export default App;
